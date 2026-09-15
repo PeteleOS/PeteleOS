caad9c6b2b40d10c8a744157a7dde47111ab83fc
tpddl08 <tpddl08@gmail.com>
2026-09-15 07:46:16 +0700

    fix reference bug

--- sys/src/libmach/obj.c
+++ sys/src/libmach/obj.c
@@ -22,30 +22,18 @@ enum
 
 int	_is2(char*),		/* in [$OS].c */
 	_is4(char*),
-	_is5(char*),
 	_is6(char*),
 	_is7(char*),
 	_is7a(char*),
-	_is8(char*),
 	_is9(char*),
-	_isk(char*),
-	_isq(char*),
-	_isv(char*),
 	_isu(char*),
-	_isi(char*),
 	_read2(Biobuf*, Prog*),
 	_read4(Biobuf*, Prog*),
-	_read5(Biobuf*, Prog*),
 	_read6(Biobuf*, Prog*),
 	_read7(Biobuf*, Prog*),
 	_read7a(Biobuf*, Prog*),
-	_read8(Biobuf*, Prog*),
 	_read9(Biobuf*, Prog*),
-	_readk(Biobuf*, Prog*),
-	_readq(Biobuf*, Prog*),
-	_readv(Biobuf*, Prog*),
-	_readu(Biobuf*, Prog*),
-	_readi(Biobuf*, Prog*);
+	_readu(Biobuf*, Prog*);
 
 typedef struct Obj	Obj;
 typedef struct Symtab	Symtab;
@@ -61,17 +49,13 @@ static Obj	obj[] =
 {			/* functions to identify and parse each type of obj */
 	[Obj68020]	"68020 .2",	_is2, _read2,
 	[ObjAmd64]	"amd64 .6",	_is6, _read6,
-	[ObjArm]	"arm .5",	_is5, _read5,
+	/* Arm(32), 386, Sparc(32), Power(32), Mips(32) and Riscv(32/64):
+	 * their obj parsers were removed along with 32-bit CPU support;
+	 * no compiler in this tree ever produces those object files. */
 	[ObjArm64]	"arm64 .7",	_is7, _read7,
 	[ObjAlpha]	"alpha .7",	_is7a, _read7a,
-	[Obj386]	"386 .8",	_is8, _read8,
-	[ObjSparc]	"sparc .k",	_isk, _readk,
-	[ObjPower]	"power .q",	_isq, _readq,
-	[ObjMips]	"mips .v",	_isv, _readv,
 	[ObjSparc64]	"sparc64 .u",	_isu, _readu,
 	[ObjPower64]	"power64 .9",	_is9, _read9,
-	[ObjRiscv]	"riscv .i",	_isi, _readi,
-	[ObjRiscv64]	"riscv64 .j",	_isi, _readi,
 	[ObjMips2]	"mips64 .4",	_is4, _read4,
 	[Maxobjtype]	0, 0
 };
